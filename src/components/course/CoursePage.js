bbafe5de00f2cf1fdfd9dec8165f1e4d3b3b9ad5
tarkey <tarkeshwarsingh560@gmail.com>
2021-07-31 23:07:31 +0530

    settingUp Reducers

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -1,8 +1,37 @@
 import React from "react";
 
-class  CoursePage extends React.Component{
- render(
-   
- )
-};
+class CoursePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      course: {
+        title: "",
+      },
+    };
+  }
+
+  handleChange = (event) => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert("hello");
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add Courses</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
+  }
+}
 export default CoursePage;
