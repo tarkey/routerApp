6008f6a1ca44cb17a279cf04a1e7c5cbc84d23f1
tarkey <tarkeshwarsingh560@gmail.com>
2021-08-01 23:23:19 +0530

    shallow and mount testing

---
+++ src/components/common/Header.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import { shallow, mount } from "enzyme";
+import { MemoryRouter } from "react-router-dom"; //memory router is needed as in reladom it requires a router as parent
+
+it("Shallow test of header", () => {
+  let navLink = shallow(<Header />).find("NavLinks").length; // searchs in raw jsx as it is shallow render
+  expect(navLink).toEqual(3);
+});
+
+it("Shallow test of header", () => {
+  let navLink = mount(
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>
+  ).find("a").length; // searchs in realdom jsx as it is shallow render
+  expect(navLink).toEqual(3);
+});
