ae44c0375f068183426a5821bb74b9c19f4e7de5
tarkey <tarkeshwarsingh560@gmail.com>
2021-08-01 16:37:08 +0530

    CourseForm Created

--- src/components/course/ManagedCoursePage.js
+++ src/components/course/ManagedCoursePage.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import { loadCourses } from "../../redux/actions/courseActions";
 import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
 
-class ManagedCoursePage extends React.Component {
-  componentDidMount() {
-    const { courses, authors, loadAuthors, loadCourses } = this.props;
+const ManagedCoursePage = ({
+  courses,
+  authors,
+  loadAuthors,
+  loadCourses,
+  ...props
+}) => {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({});
+  useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch((error) => {
         alert("Loading Courses failed" + error);
@@ -17,25 +26,26 @@ class ManagedCoursePage extends React.Component {
         alert("Loading Courses failed" + error);
       });
     }
-  }
-  render() {
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    );
-  }
-}
+  }, []);
+
+  return (
+    <>
+      <CourseForm course={course} errors={errors} authors={authors} />
+    </>
+  );
+};
 ManagedCoursePage.propType = {
   loadCourses: PropTypes.func.isRequired,
   loadAuthors: PropTypes.func.isRequired,
   courses: PropTypes.array.isRequired,
   authors: PropTypes.array.isRequired,
+  course: PropTypes.object.isRequired,
 };
 const mapStateToProps = (state) => {
   return {
     courses: state.courses,
     authors: state.authors,
+    course: newCourse,
   };
 };
 const mapDispatchToProps = {
